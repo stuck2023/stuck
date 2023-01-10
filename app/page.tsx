fd7c524e957f130f62d81b50dfe218cde6fcea63
aadarsh805 <aadarshthakur80501@gmail.com>
2023-01-10 17:10:23 +0530

    initialization

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="flex">
+      <h1 className="text-8xl underline p-4 bg-blue-500">helloo</h1>
+      <p className="text-2xl bg-white text-black">fdjksfjsdlfj</p>
+    </div>
+  );
+}
