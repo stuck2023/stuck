c048ad8c46d868475bf16c798cba926e7d91b0bd
aadarsh805 <aadarshthakur80501@gmail.com>
2023-01-13 04:24:54 +0530

    got login working

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import supabase from "../config/supabase";
 
-export default async function Home() {
-  const { data: projects }: any = await supabase
-    .from("projects")
-    .select("id, title");
+export default function Home() {
+  // const { data: projects }: any = await supabase
+  //   .from("projects")
+  //   .select("id, title");
 
-  if (!projects) return <p>no projects</p>;
+  // if (!projects) return <p>no projects</p>;
 
-  console.log(projects);
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    async function getUser() {
+      const user = await supabase.auth.getUser();
+      console.log(user);
+      setUser(user);
+    }
+    getUser();
+  }, []);
+
+  if (user) {
+    return <p>{user.data.user.email}</p>;
+  }
 
   return (
     <div>
       <h1 className="text-8xl underline p-4 bg-blue-500">helloo</h1>
       <p className="text-2xl bg-white text-black">fdjksfjsdlfj</p>
       <p className="text-2xl flex w-full bg-white text-black">
-        {projects[0].title} xDDD
+        {/* {projects[0].title} xDDD */}
       </p>
     </div>
   );
