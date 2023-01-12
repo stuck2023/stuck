657221880d2ac84d40d8b67788f85a2c54679526
aadarsh805 <aadarshthakur80501@gmail.com>
2023-01-13 02:18:53 +0530

    pushing to try magic link login

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import supabase from "../config/supabase";
 
-export default function Home() {
+export default async function Home() {
+  const { data: projects }: any = await supabase
+    .from("projects")
+    .select("id, title");
+
+  if (!projects) return <p>no projects</p>;
+
+  console.log(projects);
+
   return (
-    <div className="flex">
+    <div>
       <h1 className="text-8xl underline p-4 bg-blue-500">helloo</h1>
       <p className="text-2xl bg-white text-black">fdjksfjsdlfj</p>
+      <p className="text-2xl flex w-full bg-white text-black">
+        {projects[0].title} xDDD
+      </p>
     </div>
   );
 }
